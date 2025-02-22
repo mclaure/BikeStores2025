d30effb3e8ba3a4ba35e79b74002351d6cf9ace9
Marcelo Claure <mclaure@gmail.com>
2025-02-21 21:46:54 -0400

    changing brands PK name

--- BikeStores/BikeStoresOLTP/Schema/Tables/brands.sql
+++ BikeStores/BikeStoresOLTP/Schema/Tables/brands.sql
@@ -1,4 +1,4 @@
 ﻿CREATE TABLE production.brands (
-	brand_id INT IDENTITY (1, 1) CONSTRAINT PK_brands PRIMARY KEY,
+	brand_id INT IDENTITY (1, 1) CONSTRAINT PK_Brands PRIMARY KEY,
 	brand_name VARCHAR (255) NOT NULL
 );
